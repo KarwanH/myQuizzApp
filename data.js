4a5e4a0b4b8f39db2f8796a5817bc9be0ce94753
Karwan Hadi Lazgeen <karwanbajdi84@gmail.com>
2023-07-15 20:41:39 +0200

    fixing some issuess

---
+++ data.js
@@ -0,0 +1,92 @@
+let questions = [
+    {
+       "question" : "Welche der folgenden Sprachen ist KEINE Frontend-Sprache?",
+       "answer_1" : "HTML",
+       "answer_2" : "CSS",
+       "answer_3" : "JavaScript",
+       "answer_4" : "PHP",
+       "right_answer" : 4
+    },
+
+    {
+        "question" : "Welche Methode wird verwendet, um ein Element mit JavaScript zu selektieren?",
+        "answer_1" : "getElementById()",
+        "answer_2" : "selectElement()",
+        "answer_3" : "querySelector()",
+        "answer_4" : "findElement()",
+        "right_answer" : 1
+     },
+
+     {
+        "question" : "Welche der folgenden Aussagen über Responsive Webdesign ist korrekt?",
+        "answer_1" : "Es bezieht sich auf die Entwicklung von mobilen Apps.",
+        "answer_2" : "Es ermöglicht die Anpassung einer Website an verschiedene Geräte und Bildschirmgrößen.",
+        "answer_3" : "Es ist nur relevant für Backend-Entwickler",
+        "answer_4" : "Es beinhaltet die Verwendung von Tabellenlayouts.",
+        "right_answer" : 2
+     },
+
+     {
+        "question" : "Welches Protokoll wird verwendet, um Daten zwischen dem Webbrowser und dem Webserver zu übertragen?",
+        "answer_1" : "HTTP",
+        "answer_2" : "HTML",
+        "answer_3" : "HTTPS",
+        "answer_4" : "FTP",
+        "right_answer" : 1
+     },
+
+     {
+        "question" : "Welches der folgenden Tools wird zur Versionskontrolle in der Webentwicklung verwendet?",
+        "answer_1" : "HTML",
+        "answer_2" : "Photoshop",
+        "answer_3" : "GIT",
+        "answer_4" : "jQuery",
+        "right_answer" : 3
+     },
+
+     {
+        "question" : "Welche der folgenden CSS-Eigenschaften wird verwendet, um den Abstand zwischen Elementen zu kontrollieren?",
+        "answer_1" : "position",
+        "answer_2" : "border",
+        "answer_3" : "margin",
+        "answer_4" : "padding",
+        "right_answer" : 4
+     },
+
+     {
+        "question" : "Welche der folgenden Funktionen wird verwendet, um eine API-Anfrage mit JavaScript zu senden?",
+        "answer_1" : "fetch()",
+        "answer_2" : "sendRequest()",
+        "answer_3" : "httpRequest()",
+        "answer_4" : "ajax()",
+        "right_answer" : 1
+     },
+
+     {
+        "question" : "Welche der folgenden Aussagen über Cookies ist korrekt?",
+        "answer_1" : "Sie werden zur Verschlüsselung von Daten verwendet.",
+        "answer_2" : "Sie dienen zur Speicherung von Benutzerinformationen auf dem Client",
+        "answer_3" : "Sie werden verwendet, um JavaScript-Code auszuführen.",
+        "answer_4" : "Sie sind veraltet und werden nicht mehr verwendet.",
+        "right_answer" : 2
+     },
+
+     {
+        "question" : "Welches der folgenden Frameworks wird für die Entwicklung von Single-Page-Anwendungen verwendet?",
+        "answer_1" : "Django",
+        "answer_2" : "Bootstrap",
+        "answer_3" : "Angular",
+        "answer_4" : "Laravel",
+        "right_answer" : 3
+     },
+
+     {
+        "question" : "Welche der folgenden Dateitypen wird für animierte Grafiken im Web verwendet?",
+        "answer_1" : "GIF",
+        "answer_2" : "JPG",
+        "answer_3" : "PNG",
+        "answer_4" : "SVG",
+        "right_answer" : 1
+     }
+
+];
